da79aba8a75213656228f795be2a1661d2deff6f
perniep <pernillap94@hotmail.com>
2024-11-05 14:17:49 +0100

    Assignment 1 - Pernilla Petersson

---
+++ assignment 1 + Pernilla Petersson/scripts/script.js
@@ -0,0 +1,14 @@
+function myfunction(){
+    console.log("Divide");
+
+}
+
+let foo = 0
+let bar = 0
+
+let division = foo / bar
+
+console.log(division)
+
+if (!division)
+    alert ("You can't divide us!")
